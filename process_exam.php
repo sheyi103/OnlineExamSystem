ae6ed6dc93e7b3a4668688016a02d34b4b745ba8
sheyi103 <sheyi103@outlook.com>
2022-02-21 17:18:33 +0100

    debuging Add Exam

--- process_exam.php
+++ process_exam.php
@@ -31,7 +31,7 @@ $remainingMinutes = strtotime($examEndTime) - time();
 $currentTime = date("Y-m-d") . ' ' . date("H:i:s", STRTOTIME(date('h:i:sa')));
 include('inc/header.php');
 ?>
-<title>Online Exam System with PHP & MySQL</title>
+<title>BBC ONLINE ASSESSMENT SYSTEM</title>
 <script src="js/jquery.dataTables.min.js"></script>
 <script src="js/dataTables.bootstrap.min.js"></script>		
 <link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
@@ -41,7 +41,7 @@ include('inc/header.php');
 <script src="js/general.js"></script>
 <?php include('inc/container.php');?>
 <div class="container" style="background-color:#f4f3ef;">  
-	<h2>Online Exam System</h2>	
+	<h2>Online Exam</h2>	
 	<?php include('top_menus.php'); ?>	
 	<br>	
 	<div id="processExamId" data-exam_id="<?php echo $examDetails['id']; ?>"> 	
--- view_exam.php
+++ view_exam.php
@@ -12,7 +12,7 @@ if(!$user->loggedIn()) {
 }
 include('inc/header.php');
 ?>
-<title>Online Exam System with PHP & MySQL</title>
+<title>BBC ONLINE ASSESSMENT SYSTEM</title>
 <script src="js/jquery.dataTables.min.js"></script>
 <script src="js/dataTables.bootstrap.min.js"></script>		
 <link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
@@ -20,7 +20,7 @@ include('inc/header.php');
 <script src="js/general.js"></script>
 <?php include('inc/container.php');?>
 <div class="container" style="background-color:#f4f3ef;">  
-	<h2>Online Exam System</h2>	
+	<h2>Online Exam</h2>	
 	<?php include('top_menus.php'); ?>	
 	<br>
 	<h4>Exam List</h4>	
